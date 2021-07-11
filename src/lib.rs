1177772077386c8daa9f63228f853471c2fdc77f
Robaire Galliath <robaire@users.noreply.github.com>
2021-07-11 18:05:50 -0400

    Provide logging to the console

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,8 @@
 use wasm_bindgen::prelude::*;
 
-#[cfg(debug_assertions)]
-extern crate console_error_panic_hook;
-
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 
 #[wasm_bindgen]
 extern {
@@ -16,13 +12,17 @@ extern {
 #[wasm_bindgen]
 pub fn init() {
 
-    #[cfg(debug_assertions)]
+    #[cfg(feature = "console_error_panic_hook")]
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 
+    #[cfg(feature = "wasm-logger")]
+    wasm_logger::init(wasm_logger::Config::default());
 
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
+
+    log::info!("Some info!");
     alert(&format!("Hello, {}!", name));
 }
