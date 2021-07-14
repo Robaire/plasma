b0d9cdc2cbbc11bf1f7d5bd6687e75cbadab1e74
Robaire Galliath <robaire@users.noreply.github.com>
2021-07-14 02:25:10 -0400

    Create some basic objects

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use wasm_bindgen::prelude::*;
+use std::ops::Index;
 
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
@@ -17,12 +18,47 @@ pub fn init() {
 
     #[cfg(feature = "wasm-logger")]
     wasm_logger::init(wasm_logger::Config::default());
-
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-
     log::info!("Some info!");
     alert(&format!("Hello, {}!", name));
 }
+
+struct World {
+    origin: [f32; 3], // Mesh Origin
+    spacing: f32, // Cell Spacing
+    nodes: u32, // Number of Nodes in each Axis
+}
+
+impl World {
+    pub fn new(nodes: u32, spacing: f32) -> World {
+        World {
+            origin: [0.0, 0.0, 0.0],
+            spacing: spacing,
+            nodes: nodes
+        }
+    }
+}
+
+struct Field {
+    data: Vec<Vec<Vec<f32>>>
+}
+
+impl Field {
+    pub fn new(size: usize) -> Field {
+        Field {
+            data: vec![vec![vec![0.0; size]; size]; size]
+        }
+    }
+}
+
+impl Index<usize> for Field {
+    type Output = Vec<Vec<f32>>;
+
+    fn index(&self, i: usize) -> &Self::Output {
+        &self.data[i]
+    }
+}
+
